affeb090c2ebf37431dddea76a43d9cb81b19ee9
Parnell Springmeyer <ixmatus@gmail.com>
2013-05-07 13:13:28 -0500

    Updates to the CSS, templates and new files

---
+++ application/hakyll/static/css/Consts.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Consts where
+
+import Clay
+
+primaryColor = "#F07848" :: Color
+secondaryColor = "#C0D8D8" :: Color
+
+minorColor1 = "#3A3232" :: Color
+minorColor2 = "#D83018" :: Color
+minorColor3 = "#FDFCCE" :: Color
+
